805b48ce2fe7230e44ff9a5f7f1c487a4b7c46c4
Vitor Alano <vitor.alano@nodo.cc>
2021-03-21 19:51:56 -0300

    Scopes

--- api/app/models/card.rb
+++ api/app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ApplicationRecord
   belongs_to :edition
+
+  scope :red, -> { where("color like '%R%'") }
+  scope :white, -> { where("color like '%W%'") }
+  scope :sorcery, -> { where(card_type: 'Sorcery') }
+  scope :boros, -> { red.white }
 end
